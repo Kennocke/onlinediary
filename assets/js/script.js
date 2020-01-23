36defb22b3e3315d7dbed8f5172d93397676f405
Kennocke <32141916+Kennocke@users.noreply.github.com>
2020-01-24 01:15:59 +0300

    Загрузка

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+$(function () {
+  // метод cycle
+  $('#next').click(function () {
+      $('#carousel').carousel('next');
+  });
+
+   $('#prev').click(function () {
+      $('#carousel').carousel('prev');
+  });
+});
